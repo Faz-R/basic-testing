57bd00d83f5a3b015c317469ae887b823ede42f4
Faz-R <FazaR92@gmail.com>
2023-06-30 13:58:29 +0300

    feat: add snapshot

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,20 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+// import axios from 'axios';
+// import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
     // Write your test here
+    // const relativePath = '/test';
+    // jest.mock('axios', () => ({
+    //   create: jest.fn(() => ({
+    //     get: jest.fn(),
+    //   })),
+    // }));
+    // await throttledGetDataFromApi(relativePath);
+    // expect(axios.create).toHaveBeenCalledWith({
+    //   baseURL: 'https://jsonplaceholder.typicode.com',
+    // });
   });
 
   test('should perform request to correct provided url', async () => {
